e362a51e0e6e113a2899f230b2107b11d3cd7298
jhimydevg <chance.jhimy@gmail.com>
2024-09-25 11:38:57 -0400

    test unitarios

---
+++ src/main/java/com/jhimydev/javatests/util/StringUtil.java
@@ -0,0 +1,14 @@
+package com.jhimydev.javatests.util;
+
+public class StringUtil {
+
+    public static String repeat(String str, int times) {
+        String result="";
+
+        for (int i = 0; i < times; i++) {
+            result += str;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/jhimydev/javatests/util/StringUtilTest.java
@@ -0,0 +1,21 @@
+package com.jhimydev.javatests.util;
+
+
+
+public class StringUtilTest {
+    public static void main(String[] args) {
+        String result = StringUtil.repeat("hola", 3);
+
+
+        if (!result.equals("holaholahola")) {
+            System.out.println("ERROR");
+        }
+
+        String result2 = StringUtil.repeat("hola", 2);
+
+        if (!result2.equals("holahola")) {
+            System.out.println("ERROR");
+        }
+    }
+
+}
