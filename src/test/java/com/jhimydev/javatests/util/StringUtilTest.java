8a0565510b965b04cb30a80c47de67c90265df8b
jhimydevg <chance.jhimy@gmail.com>
2024-09-25 12:08:08 -0400

    Creación de test unitario: lanzar una excepción para alertar sobre un error

--- src/test/java/com/jhimydev/javatests/util/StringUtilTest.java
+++ src/test/java/com/jhimydev/javatests/util/StringUtilTest.java
@@ -4,17 +4,15 @@ package com.jhimydev.javatests.util;
 
 public class StringUtilTest {
     public static void main(String[] args) {
-        String result = StringUtil.repeat("hola", 3);
 
+        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
 
-        if (!result.equals("holaholahola")) {
-            System.out.println("ERROR");
-        }
+        assertEquals(StringUtil.repeat("hola", 1), "hola");
+    }
 
-        String result2 = StringUtil.repeat("hola", 2);
-
-        if (!result2.equals("holahola")) {
-            System.out.println("ERROR");
+    private static void assertEquals(String actual, String expected) {
+        if (!actual.equals(expected)) {
+            throw new RuntimeException(actual + " is not equal to expected" +expected);
         }
     }
 
