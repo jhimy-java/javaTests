66edb36721b683969bbd6fe23c9c218d3c2afa34
jhimydevg <chance.jhimy@gmail.com>
2024-09-25 16:09:41 -0400

    Organización de tests con JUnit

--- src/main/java/com/jhimydev/javatests/util/StringUtil.java
+++ src/main/java/com/jhimydev/javatests/util/StringUtil.java
@@ -3,6 +3,11 @@ package com.jhimydev.javatests.util;
 public class StringUtil {
 
     public static String repeat(String str, int times) {
+        if (times < 0)
+        {
+            throw new IllegalArgumentException("negative times not allowed");
+        }
+
         String result="";
 
         for (int i = 0; i < times; i++) {
