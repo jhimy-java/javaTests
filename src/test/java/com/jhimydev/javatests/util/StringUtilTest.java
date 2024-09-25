087c6e141fda9ca256b6c537ad63933d8a6200d2
jhimydevg <chance.jhimy@gmail.com>
2024-09-25 14:45:47 -0400

    Test unitario con JUnit

--- src/test/java/com/jhimydev/javatests/util/StringUtilTest.java
+++ src/test/java/com/jhimydev/javatests/util/StringUtilTest.java
@@ -1,19 +1,19 @@
 package com.jhimydev.javatests.util;
 
 
+import org.junit.Assert;
+import org.junit.Test;
 
 public class StringUtilTest {
-    public static void main(String[] args) {
 
-        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
+    @Test
+    public void testRepeat() {
 
-        assertEquals(StringUtil.repeat("hola", 1), "hola");
+        Assert.assertEquals("holaholahola",StringUtil.repeat("hola", 3));
+
+        Assert.assertEquals("hola",StringUtil.repeat("hola", 1));
     }
 
-    private static void assertEquals(String actual, String expected) {
-        if (!actual.equals(expected)) {
-            throw new RuntimeException(actual + " is not equal to expected" +expected);
-        }
-    }
+
 
 }
