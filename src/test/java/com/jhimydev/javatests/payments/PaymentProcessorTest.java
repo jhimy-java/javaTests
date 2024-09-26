dbda7ee343f16449b6da9d9bc78543856496d176
jhimydevg <chance.jhimy@gmail.com>
2024-09-25 20:36:47 -0400

    Análisis de los tests y mejoras

--- src/test/java/com/jhimydev/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/jhimydev/javatests/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.jhimydev.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,13 +8,23 @@ import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
 
+    PaymentGateway paymentGateway;
+    PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setUp(){
+         paymentGateway = Mockito.mock(PaymentGateway.class);
+         paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
+
     @Test
     public void payment_is_correct() {
         //preparación del escenario
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
         //la llamada al método que queremos probar
         boolean result = paymentProcessor.makePayment(1000);
         //la comprobación de que el resultado es el esperado
@@ -23,11 +34,9 @@ public class PaymentProcessorTest {
     @Test
     public void payment_is_wrong() {
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         boolean result = paymentProcessor.makePayment(1000);
 
