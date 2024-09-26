98cc9ab98f8fef155e9eca8bbf04e8da73de17b5
jhimydevg <chance.jhimy@gmail.com>
2024-09-25 20:31:17 -0400

    comentando las partes que tiene un test en promedio SLR

--- src/test/java/com/jhimydev/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/jhimydev/javatests/payments/PaymentProcessorTest.java
@@ -9,14 +9,15 @@ public class PaymentProcessorTest {
 
     @Test
     public void payment_is_correct() {
-
+        //preparación del escenario
         PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-
         PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-
-        assertTrue(paymentProcessor.makePayment(1000));
+        //la llamada al método que queremos probar
+        boolean result = paymentProcessor.makePayment(1000);
+        //la comprobación de que el resultado es el esperado
+        assertTrue(result);
     }
 
     @Test
